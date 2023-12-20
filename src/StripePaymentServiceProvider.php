9ab5d5bec56e8a697c6207435e50b6abe66a82a2
Md. Kamrul Haque <72596604+Kamrul-Haque@users.noreply.github.com>
2023-12-20 11:44:03 +0600

    Publishing error fixed

--- src/StripePaymentServiceProvider.php
+++ src/StripePaymentServiceProvider.php
@@ -27,7 +27,7 @@ class StripePaymentServiceProvider extends ServiceProvider
             __DIR__ . '/../config/stripe.php' => config_path('stripe.php'),
             __DIR__ . '/../stubs/database/migrations/create_stripe_payments_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_stripe_payments_table.php'),
             __DIR__ . '/../stubs/Models/StripePayment.php' => app_path('Models/StripePayment.php'),
-            __DIR__ . '/../Stubs/Http/Controllers/StripePaymentController.php' => app_path('Http/Controllers/StripePaymentController.php'),
+            __DIR__ . '/../stubs/Http/Controllers/StripePaymentController.php' => app_path('Http/Controllers/StripePaymentController.php'),
             __DIR__ . '/../resources/views' => resource_path('views/stripe-payments/'),
             __DIR__ . '/../public' => public_path('vendor/laravel-stripe-payment'),
             __DIR__ . '/../stubs/routes/stripe.php' => base_path('routes/stripe.php'),
